9554d177ea55731e9b44b3cd8a6bbb10c7725227
MrRinkuKumar <you@example.com>
2022-11-28 00:31:44 +0530

    formatted

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {MdCall, MdPendingActions} from 'react-icons/md'
+import { MdCall, MdPendingActions } from "react-icons/md";
 // import styles from '../styles/login.module.css'
 const Login = () => {
   const [user, setUsers] = useState([]);
@@ -21,51 +21,71 @@ const Login = () => {
   console.warn(user);
   return (
     <>
-      
-      
-	  
-	  <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Users</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Banh mi cornhole echo park skateboard authentic crucifix neutra tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon twee</p>
-    </div>
-    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-      <table class="table-auto w-full text-left whitespace-no-wrap">
-        <thead>
-          <tr>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">S.no</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Name</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Email</th>
-            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Status</th>
-            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
-          </tr>
-        </thead>
-        <tbody>
-		{user.map((item,index)=>
-			<tr>
-            <td class="px-4 py-3">{index}</td>
-            <td class="px-4 py-3">{item.name}</td>
-            <td class="px-4 py-3">{item.email}</td>
-            <td class="px-4 py-3 text-lg text-gray-900">pending</td>
-            <td class="w-10 text-center">
-			<MdCall/>
-            </td>
-          </tr>
-
-		)}
-        </tbody>
-      </table>
-    </div>
-    <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
-      <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"> More
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-          <path d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg>
-      </a>
-    </div>
-  </div>
-</section>
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-col text-center w-full mb-20">
+            <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
+              Users
+            </h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Banh mi cornhole echo park skateboard authentic crucifix neutra
+              tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon
+              twee
+            </p>
+          </div>
+          <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+            <table class="table-auto w-full text-left whitespace-no-wrap">
+              <thead>
+                <tr>
+                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                    S.no
+                  </th>
+                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                    Name
+                  </th>
+                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                    Email
+                  </th>
+                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                    Status
+                  </th>
+                  <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {user.map((item, index) => (
+                  <tr>
+                    <td class="px-4 py-3">{index}</td>
+                    <td class="px-4 py-3">{item.name}</td>
+                    <td class="px-4 py-3">{item.email}</td>
+                    <td class="px-4 py-3 text-lg text-gray-900">pending</td>
+                    <td class="w-10 text-center">
+                      <MdCall />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+            <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+              {" "}
+              More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
